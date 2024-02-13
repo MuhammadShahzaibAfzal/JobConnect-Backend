40f533651ebb46c6933dee12cbeec60de0679485
Shahzaib <shahzaibafzalofficial@gmail.com>
2024-02-13 22:39:26 +0500

    updated error handling middleware and service

--- src/middlewares/globalErrorHandler.js
+++ src/middlewares/globalErrorHandler.js
@@ -1,25 +1,36 @@
+import { logger } from "../config/logger.js";
 import { ErrorHandlerService } from "../services/ErrorHandlerService.js";
 
 export const globalErrorHandlerMiddleware = (err, req, res, next) => {
   let statusCode = 500;
+  const isProduction = process.env.NODE_ENV === "production";
+  const message = isProduction ? "Internal server error" : err.message;
+
+  // logging error
+  logger.log("error", err.message, {
+    statusCode,
+    error: err.stack,
+    path: req.path,
+    method: req.method,
+  });
+
   let data = {
-    message: "Internal Server Error",
-    ...((process.env.NODE_ENV === "development" ||
-      process.env.NODE_ENV === "test") && {
-      originalMessage: err.message,
-    }),
+    errors: [
+      {
+        type: err.name,
+        msg: message,
+        path: req.path,
+        method: req.method,
+        location: "server",
+        stack: isProduction ? null : err.stack,
+      },
+    ],
   };
 
   if (err instanceof ErrorHandlerService) {
     statusCode = err.status;
-    data = {
-      message: err.message,
-    };
+    data.errors[0].msg = err.message;
   }
-
-  console.log("====================================");
-  console.log(data);
-  console.log("====================================");
 
   return res.status(statusCode).json(data);
 };
--- src/services/ErrorHandlerService.js
+++ src/services/ErrorHandlerService.js
@@ -6,22 +6,22 @@ export class ErrorHandlerService extends Error {
   }
 
   static notFoundError(message = "Not Found") {
-    return ErrorHandlerService(404, message);
+    return new ErrorHandlerService(404, message);
   }
 
   static alreadyExistError(message = "Allready Exist") {
-    return ErrorHandlerService(409, message);
+    return new ErrorHandlerService(409, message);
   }
 
   static unAuthorizedError(message = "UnAuthorized") {
-    return ErrorHandlerService(401, message);
+    return new ErrorHandlerService(401, message);
   }
 
   static badRequestError(message = "Bad Request") {
-    return ErrorHandlerService(400, message);
+    return new ErrorHandlerService(400, message);
   }
 
   static forbiddenError(message = "Not Allowed") {
-    return ErrorHandlerService(403, message);
+    return new ErrorHandlerService(403, message);
   }
 }
