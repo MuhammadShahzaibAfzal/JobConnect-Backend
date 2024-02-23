ab8b39a741fd95461a8a76cf83d6f2d3f2fcf486
Shahzaib <shahzaibafzalofficial@gmail.com>
2024-02-23 20:45:07 +0500

    add job model

---
+++ src/models/jobModel.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema(
+  {
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    jobNature: {
+      type: String,
+      required: true,
+    },
+    vacancy: {
+      type: Number,
+      required: true,
+    },
+    salary: {
+      min: {
+        type: Number,
+        required: true,
+      },
+      max: {
+        type: Number,
+        required: true,
+      },
+      currency: {
+        type: String,
+        required: true,
+      },
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+    },
+    skillsRequired: [String],
+
+    requirements: {
+      type: String,
+    },
+    responsibilites: {
+      type: String,
+    },
+    comapanyName: {
+      type: String,
+      required: true,
+    },
+    comapanyWebsite: {
+      type: String,
+    },
+    expirationDate: Date,
+    status: {
+      type: String,
+      enum: ["active", "filled", "expired", "paused"],
+      default: "active",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const JobModel = mongoose.model("Job", jobSchema);
+
+export default JobModel;
